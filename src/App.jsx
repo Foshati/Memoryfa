de7b653dd691bd69e66fe81c4417b3f246a83cd9
Foshati <amirrezafoshati@gmail.com>
2024-03-12 23:33:24 +0330

    add style and import image

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import "./App.css";
 
+const cardImages = [
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
+];
+
 export default function App() {
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages];
+    console.log(shuffledCards);
+  };
   return (
-    <h1 className="text-3xl text-red-200 font-bold underline">Hello world!</h1>
+    <div>
+      <h1 className="m-8 text-5xl font-bold text-yellow-200">Memoryfa</h1>
+      <button
+        className="px-2 py-4 m-4 rounded-2xl bg-slate-600"
+        onClick={shuffleCards}
+      >
+        New game
+      </button>
+    </div>
   );
 }
